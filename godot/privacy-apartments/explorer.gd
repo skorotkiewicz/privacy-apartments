b6cf15a36212e64d9002934a291aa2726289f6e5
Sebastian Korotkiewicz <skorotkiewicz@gmail.com>
2026-07-20 01:10:58 +0200

    Convert explorer to physics-based character controller

--- godot/privacy-apartments/explorer.gd
+++ godot/privacy-apartments/explorer.gd
@@ -1,12 +1,17 @@
-extends Node3D
+extends CharacterBody3D
 
-@export var speed := 8.0
+@export var speed := 6.0
 @export var look_sensitivity := 0.002
 
 @onready var camera: Camera3D = $Camera3D
+@onready var apartments: Node3D = $"../Apartments"
 
 func _ready() -> void:
 	camera.make_current()
+	look_at(Vector3(0, global_position.y, 0))
+	for node in apartments.get_children():
+		if node is MeshInstance3D:
+			node.create_trimesh_collision()
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _unhandled_input(event: InputEvent) -> void:
@@ -14,17 +19,21 @@ func _unhandled_input(event: InputEvent) -> void:
 		rotate_y(-event.relative.x * look_sensitivity)
 		camera.rotate_x(-event.relative.y * look_sensitivity)
 		camera.rotation.x = clamp(camera.rotation.x, -1.5, 1.5)
+	elif event.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = 4.5
 	elif event.is_action_pressed("ui_cancel"):
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	elif event is InputEventMouseButton and event.pressed:
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 	var right := camera.global_transform.basis.x
 	var forward := -camera.global_transform.basis.z
 	right.y = 0.0
 	forward.y = 0.0
-	var movement := right.normalized() * (float(Input.is_physical_key_pressed(KEY_D)) - float(Input.is_physical_key_pressed(KEY_A)))
-	movement += forward.normalized() * (float(Input.is_physical_key_pressed(KEY_W)) - float(Input.is_physical_key_pressed(KEY_S)))
-	movement.y = float(Input.is_physical_key_pressed(KEY_SPACE)) - float(Input.is_physical_key_pressed(KEY_SHIFT))
-	global_position += movement.normalized() * speed * delta
+	var direction := (right.normalized() * (float(Input.is_physical_key_pressed(KEY_D)) - float(Input.is_physical_key_pressed(KEY_A))) + forward.normalized() * (float(Input.is_physical_key_pressed(KEY_W)) - float(Input.is_physical_key_pressed(KEY_S)))).normalized()
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	move_and_slide()
